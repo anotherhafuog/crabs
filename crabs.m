ec65576621c95ce08a747858f58e731df8f0e50e
anotherhafuog <freesolokills@gmail.com>
2023-10-27 14:35:12 -0700

    added code for catching crab and taking damage

--- crabs.m
+++ crabs.m
@@ -3,30 +3,48 @@ function crabs (level)
 % hunts for a very clever and powerful crab.
 % Draw the game map and initialize map dimensions.
 [mapHeight , mapWidth] = drawMap( "BGImage.png" );
-% Initialize captain location, heading and size
+%initialize captain location, heading and size
 xCapt = 1000;
 yCapt = 500;
 thetaCapt = -pi/2;
 sizeCapt = 50;
+healthCapt = 100;
+crabsCaught = 0;
 %initialize crab location, heading and size
 xCrab = 1000;
 yCrab = 1200;
 thetaCrab = -pi/2;
 sizeCrab = 50;
-%initialize jellyfish
-xJelly = rand*mapWidth;
-yJelly = 0;
+%initialize jelly fish
+xJelly=rand*mapWidth;
+yJelly=0;
 thetaJelly = -pi/2;
 sizeJelly = 25;
+jellySting = 2;
 % Draw the captain and initialize graphics handles
 %*********************************************************
 % Put your call to drawCapt() here ..... You must give drawCapt its
 % input and output arguments.
-captGraphics = drawCapt(xCapt, yCapt, thetaCapt, sizeCapt);
+[captGraphics,xNet,yNet] = drawCapt(xCapt,yCapt,thetaCapt,sizeCapt);
 crabGraphics = drawCrab(xCrab,yCrab,thetaCrab,sizeCrab);
 jellyGraphics = drawJelly(xJelly,yJelly,thetaJelly,sizeJelly);
 %*******************************************************
+% print health status
+healthLoc = [100,100];
+crabsCaughtLoc = [100,175];
+healthStatus = text(healthLoc(1), healthLoc(2), strcat('Health = ', ...
+num2str(healthCapt)), 'FontSize', 12, 'Color', 'red');
+crabsCaughtStatus = text(crabsCaughtLoc(1), crabsCaughtLoc(2), ...
+strcat('Crabs Caught = ',num2str(crabsCaught)), 'FontSize', 12, 'Color', 'red');
 while(1)
+%remove old and plot new health and points status to screen
+delete(healthStatus);
+delete(crabsCaughtStatus);
+healthStatus = text(healthLoc(1), healthLoc(2), strcat('Health = ', ...
+num2str(healthCapt)), 'FontSize', 12, 'Color', 'red');
+crabsCaughtStatus = text(crabsCaughtLoc(1), crabsCaughtLoc(2),
+strcat('Crabs Caught = ', ...
+num2str(crabsCaught)), 'FontSize', 12, 'Color', 'red');
 % erase old jellyfish
 for i=1:length(jellyGraphics)
 delete(jellyGraphics(i));
@@ -48,7 +66,7 @@ endfor
 % move capt
 [xCapt, yCapt, thetaCapt] = moveCapt(cmd, xCapt, yCapt, thetaCapt, mapWidth, mapHeight);
 % draw new capt
-captGraphics = drawCapt( xCapt, yCapt, thetaCapt, sizeCapt);
+[captGraphics,xNet,yNet] = drawCapt(xCapt,yCapt,thetaCapt,sizeCapt);
 elseif (cmd == "i" || cmd == "j" || cmd == "k" || cmd == "l" || cmd ==",") % respond crab moved
 %erase old crab
 for i=1:length(crabGraphics)
@@ -58,6 +76,24 @@ endfor
 [xCrab,yCrab,thetaCrab] = moveCrab(cmd,xCrab,yCrab,thetaCrab,mapHeight,
 mapWidth);
 %draw new captain and crab
+crabGraphics = drawCrab(xCrab,yCrab,thetaCrab,sizeCrab);
+endif
+if ( getDist(xJelly,yJelly,xCapt,yCapt) < 3*sizeCapt )
+healthCapt = healthCapt - jellySting;
+endif
+if( getDist(xNet,yNet,xCrab,yCrab) < 2*sizeCapt ) %crab is caught
+%keep track of how many crabs are caught
+crabsCaught = crabsCaught +1;
+%erase old crab
+for i=1:length(crabGraphics)
+delete(crabGraphics(i));
+endfor
+%create a new crab. initialize new crab location, heading and size
+xCrab = rand*mapWidth;
+yCrab = rand*mapHeight;
+thetaCrab = -pi/2;
+sizeCrab = 50;
+%draw new crab
 crabGraphics = drawCrab(xCrab,yCrab,thetaCrab,sizeCrab);
 endif
 fflush(stdout);
--- drawCapt.m
+++ drawCapt.m
@@ -1,4 +1,4 @@
-function captainGraphics = drawCapt (xCapt , yCapt , thetaCapt , sizeCapt)
+function [captainGraphics,xNet,yNet] = drawCapt(xCapt,yCapt,thetaCapt,sizeCapt)
 % In the future, this function will draw the captain at the given
 % position (xCapt , yCapt) , with heading thetaCapt.
 % For now, it draws the captain at the (0, 0) with 0 heading.
@@ -33,6 +33,9 @@ pt11=capt( : , 11);
 pt12=capt( : , 12);
 pt13=capt( : , 13);
 pt14=capt( : , 14);
+%Set the spearhead point
+xNet = pt13(1);
+yNet = pt13(2);
 % Draw the captain and set the return vector of graphics handles.
 captainGraphics(1) = drawLine(pt1 , pt2 , "k");
 captainGraphics(2) = drawLine(pt2 , pt3 , "k");
